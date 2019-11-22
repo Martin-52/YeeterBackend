0dde9d913b718923d2631e186a160158eb55c9fa
Martin-52 <m.landin5258@gmail.com>
2019-11-22 11:02:07 -0800

    Initial Commit

---
+++ src/main/java/com/yeeter/web/YeeterWebBackend/YeeterWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yeeter.web.YeeterWebBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeeterWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeeterWebBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeeter/web/YeeterWebBackend/YeeterWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeeter.web.YeeterWebBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeeterWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
