8d108c077dc74be88a30d253ad03e8eb6922557c
Martin-52 <m.landin5258@gmail.com>
2019-11-25 11:52:16 -0800

    Firebase Configuration

--- src/main/java/com/yeeter/web/YeeterWebBackend/YeeterWebBackendApplication.java
+++ src/main/java/com/yeeter/web/YeeterWebBackend/YeeterWebBackendApplication.java
@@ -1,13 +1,36 @@
 package com.yeeter.web.YeeterWebBackend;
 
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class YeeterWebBackendApplication {
 
+	static String db_url = "";
 	public static void main(String[] args) {
 		SpringApplication.run(YeeterWebBackendApplication.class, args);
+		
+		try {
+			FirebaseOptions options = new FirebaseOptions.Builder()
+					.setCredentials(GoogleCredentials
+							.fromStream(new ClassPathResource("/yeeter-backend.json").getInputStream()))
+					.setDatabaseUrl("https://yeeter-backend.firebaseio.com/")
+					.build();
+			
+			FirebaseApp defaultApp = FirebaseApp.initializeApp(options);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
