ca4f9faaeb6d4a1a86cdaedcc52a739754a177ce
Martin-52 <m.landin5258@gmail.com>
2019-12-10 20:29:15 -0800

    Changes

--- src/main/java/com/yeeter/web/YeeterWebBackend/controller/UserController.java
+++ src/main/java/com/yeeter/web/YeeterWebBackend/controller/UserController.java
@@ -4,11 +4,13 @@ import com.google.firebase.auth.FirebaseAuthException;
 import com.yeeter.web.YeeterWebBackend.service.UserService;
 import com.yeeter.web.YeeterWebBackend.service.YeetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class UserController {
     @Autowired
